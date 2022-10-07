020b8f93798c8091e0709b86fa1e5845e961feda
Bouncy0Knight <priyanshu.alluri.26@dartmouth.edu>
2022-10-06 20:19:30 -0400

    djfhsdkjf

--- src/App.js
+++ src/App.js
@@ -2,6 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Priyanshu Alluri'
+  let variable = "";
+  if (name === "Camden"){
+    variable = (<p>This is camden</p>);
+  }
+  let nameString = name === "Ben" ?"this i ben!" :"this is not ben";
+
+  const buttonClick = () => {
+    console.log("I clicked the button");
+  }
+
+  const onInputChange = (event) => {
+    console.log(event)
+    console.log(event.target.value);
+  }
+
+  const list = [1,2,3,4];
+
+  const listPlusOne = list.map(number => {
+    return number + 1;
+
+  const siblings = ["Kelsie", "Leland"];
+
+  const siblingsMap = siblings.map((sibling) => {
+    return <p> This siblings name is {sibling} </p>
+});
+
+
+})
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +38,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p> Hi, my name is {name} </p>
+        {nameString}
+        {
+          console.log(listPlusOne)
+        }
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -16,7 +50,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> <button onClick = {buttonClick}>Click me</button>
+        <input type ="text" onChange = {onInputChange} />
       </header>
     </div>
   );
